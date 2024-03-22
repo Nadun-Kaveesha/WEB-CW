d5b10e87b1a66b1f54bb97655f75f779163273cd
Ashen-Maneth <manethpamuditha23b@gmail.com>
2024-03-22 10:27:56 +0530

    hiii

--- HomePage/index.js
+++ HomePage/index.js
@@ -40,5 +40,6 @@ if (window.innerWidth <= 720) {
   
   document.getElementById("Gallery").addEventListener("click", function () {
     window.location.href = "/Gallery/Gallery.html";
+    
   });
   
